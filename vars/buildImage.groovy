dcd551afbee72961a0dd53390e3d5a85568a0ede
Utkal Nayak <nayak.utkal@gmail.com>
2021-12-10 10:48:01 -0500

    added shared lib classes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,18 +1,6 @@
 #!/usr/bin/env groovy
+import com.learn.jenkins.DockerImage
 
 def call(String imageName) {
-    echo "Build Docker image for basic java app and push to docker hub ..."
-
-    // use docker hub credentials from jenkins credentials settings
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-utkal', passwordVariable: 'PSWD', usernameVariable: 'UID')]) {
-        // command to build an image with a tag (version)
-        // image name must match with private repo name in docker hub
-        sh "docker build -t $imageName ."
-
-        // authenticate to docker hub
-        sh "echo $PSWD | docker login -u $UID --password-stdin"
-
-        // push image to docker hub
-        sh "docker push $imageName"
-    }
+    return new DockerImage(this).buildDockerImage(imageName)   
 }
