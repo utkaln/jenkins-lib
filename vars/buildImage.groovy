051b5adcdccb62d2f470bfd928a1572bad25a5c3
Utkal Nayak <nayak.utkal@gmail.com>
2021-12-12 09:44:12 -0500

    updated string operation to update version

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 import DockerImage
 
 def call(String imageName) {
-    echo "Ready to build image with version --> $env.IMAGE_TAG"
-    def completeImageVersion = imageName+$env.IMAGE_TAG
+    def completeImageVersion = new StringBuffer().append(imageName).append(env.IMAGE_TAG)
+    echo "Ready to build image with version --> $completeImageVersion"
     return new DockerImage(this).buildDockerImage(completeImageVersion)   
 }
