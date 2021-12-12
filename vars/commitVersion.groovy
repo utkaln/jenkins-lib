76e496cb4b4a52eb909b918f3a6371acebe75bf8
Utkal Nayak <nayak.utkal@gmail.com>
2021-12-12 13:04:21 -0500

    removed param from the commitVersion method

--- vars/commitVersion.groovy
+++ vars/commitVersion.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 import DockerImage
 
-def call(String versionName) {
-    return new DockerImage(this).commitVersionNum(versionName)   
+def call() {
+    return new DockerImage(this).commitVersionNum()   
 }
