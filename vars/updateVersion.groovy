83543e11d246f4cabce1689105f1eb5260a42292
Utkal Nayak <nayak.utkal@gmail.com>
2021-12-12 09:16:49 -0500

    modified file to accept dynamic version name

---
+++ vars/updateVersion.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+def call() {
+    echo "Updating version of the build under - $BRANCH_NAME ..."
+    sh 'mvn build-helper:parse-version versions:set \
+    -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+    versions:commit'
+    def version_line_pom = readFile('pom.xml') =~ '<version>(.+))</version>'
+    version_line_pom[0][1]
+    env.IMAGE_TAG = "$version_line_pom-$BUILD_NUMBER"
+    echo "The new version of the Docker Image is going to be $env.IMAGE_TAG"
+}
