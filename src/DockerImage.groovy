d62f382a962d85bba96de27779e4a94a1daeb4d0
Utkal Nayak <nayak.utkal@gmail.com>
2021-12-14 23:00:03 -0500

    param sent to deploy method for ip

--- src/DockerImage.groovy
+++ src/DockerImage.groovy
@@ -52,7 +52,7 @@ class DockerImage implements Serializable {
 
     def deployToEC2(String ipEC2) {
          
-        script.echo "ip addr found as $sipEC2"
+        script.echo "ip addr found as $script.ipEC2"
         def dockerRunCmd = 'docker run -p 8080:8080 -d utkal/demo-java-maven-app:lts'
         script.sshagent(['ec2-server-key']) {
             // IP subject to change with each restart of EC2
