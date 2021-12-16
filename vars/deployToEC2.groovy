1e7ffe40ad47eaab54b7b63a2e55dc0a0447e2fa
Utkal Nayak <nayak.utkal@gmail.com>
2021-12-15 20:48:02 -0500

    accepting image name as param

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 import DockerImage
 
-def call(String ec2ip) {
-    return new DockerImage(this).deployToEC2(ec2ip, env.IMAGE_TAG)   
+def call(String ec2ip, String imageName) {
+    return new DockerImage(this).deployToEC2(ec2ip, imageName, env.IMAGE_TAG)   
 }
