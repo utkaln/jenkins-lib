f11d03135cecce717382973fc5845c466fa13244
Utkal Nayak <nayak.utkal@gmail.com>
2021-12-15 19:25:30 -0500

    reading image tag from env var

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 import DockerImage
 
-def call(String ec2ip) {
-    return new DockerImage(this).deployToEC2(ec2ip)   
+def call(String ec2ip, String imageTag) {
+    return new DockerImage(this).deployToEC2(ec2ip, imageTag)   
 }
