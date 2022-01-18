7e7a776a602499f1284d52ffa2fe0f8f18dbe9ce
Utkal Nayak <nayak.utkal@gmail.com>
2022-01-18 16:08:05 -0500

    clean up files after terraform ec2 changes

--- vars/deployToEC2Terraform.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-import DockerImage
-
-def call(String imageName) {
-    return new DockerImage(this).deployToEC2Terraform(imageName, env.IMAGE_TAG)   
-}
--- vars/provisionEC2Terraform.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-import DockerImage
-
-def call() {
-    return new DockerImage(this).provisionEC2Terraform()   
-}
